6d5c1cddca5c30a43fcf735c2be7e9561258434f
andymurd <andy@andymurdoch.com>
2024-01-11 18:03:29 +1030

    Boilerplate

---
+++ src/scene.ts
@@ -0,0 +1,18 @@
+//import { createPlaneMarker } from "./objects/PlaneMarker";
+import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+//import { handleXRHitTest } from "./utils/hitTest";
+
+import {
+  AmbientLight,
+  BufferGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  Object3D,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
+
+export function createScene(renderer: WebGLRenderer) {
+  // TODO: Create a scene and build a WebXR app!
+}
